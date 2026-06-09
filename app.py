c7ac0d6be4eb4f2fb7e7986491fbe174667c10e7
Suman Kapri <kaprisuman222@gmail.com>
2026-06-09 15:54:56 +0545

    Add resume form, preview page and styling

--- app.py
+++ app.py
@@ -1,10 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import sqlite3
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/preview", methods=["POST"])
+def preview():
+
+    data = {
+    "name": request.form.get("name"),
+    "email": request.form.get("email"),
+    "phone": request.form.get("phone"),
+    "linkedin": request.form.get("linkedin"),
+    "github": request.form.get("github"),
+    "education": request.form.get("education"),
+    "skills": request.form.get("skills"),
+    "projects": request.form.get("projects"),
+    "experience": request.form.get("experience"),
+    "summary": request.form.get("summary"),
+    "certifications": request.form.get("certifications")
+}
+    
+
+    return render_template("preview.html", data=data)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
